8826faf3d373b556ffd76e28b56777da00774f26
Max Göttlicher <max.goettlicher@gmx.de>
2022-08-01 13:33:42 +0200

    reading graphs properly

--- include/pds.hpp
+++ include/pds.hpp
@@ -5,8 +5,11 @@
 #ifndef PDS_PDS_HPP
 #define PDS_PDS_HPP
 
-#include "boost/graph/adjacency_list.hpp"
-#include "boost/graph/dijkstra_shortest_paths.hpp"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphml.hpp>
+#include <boost/property_map/dynamic_property_map.hpp>
+
+#include <fstream>
 
 #include "utility.hpp"
 
@@ -17,97 +20,122 @@ template<class T>
 void print_type(T&&) { PrintType<T>{}; }
 
 namespace pds {
-    enum class PmuState {
-        Blank,
-        Active,
-        Inactive
-    };
 
-    template<typename T> struct PrintType;
-    template<class Graph, class ActivePropertyMap, class ObservedPropertyMap>
-    bool dominate(const Graph& graph, const ActivePropertyMap& active, ObservedPropertyMap& observed) {
-        typename boost::property_map<Graph, boost::vertex_index_t>::type index = get(boost::vertex_index, graph);
-        std::for_each(vertices(graph).first, vertices(graph).second, [&] (const auto& v) {
-            auto x = index[v];
+enum class PmuState {
+    Blank,
+    Active,
+    Inactive
+};
 
-            if (active[v] == PmuState::Active) {
-                observed[v] = true;
-                auto adjacent = boost::adjacent_vertices(v, graph);
-                for (auto it = adjacent.first; it != adjacent.second; ++it) {
-                    auto windex = index[*it];
-                    observed[*it] = true;
-                }
-            }
-        });
-        return true;
+struct Bus {
+    std::string id;
+    bool zero_injection;
+};
+
+using PowerGrid = boost::adjacency_list<boost::listS, boost::listS, boost::undirectedS, Bus>;
+
+PowerGrid read_graphml(const std::string& filename, bool all_zero_injection = false) {
+    PowerGrid graph;
+    boost::dynamic_properties attr(boost::ignore_other_properties);
+    std::vector<long> long_zi(num_vertices(graph));
+    //typename boost::property_map<PowerGrid, boost::vertex_index_t>::type index = get(boost::vertex_index, graph); //
+    boost::associative_property_map<std::map<PowerGrid::vertex_descriptor, long>> zero_injection;
+    attr.property("zero_injection", zero_injection);//make_vector_property_map(long_zi, graph));
+    std::ifstream graph_in(filename);
+    boost::read_graphml(graph_in, graph, attr);
+    graph_in.close();
+    for (auto it = vertices(graph).first; it != vertices(graph).second; ++it) {
+        auto v = *it;
+        graph[v].zero_injection = true;//zero_injection[v];
     }
+    return graph;
+}
 
-    template<class Graph, class ObservedPropertyMap>
-    std::vector<typename Graph::vertex_descriptor> propagation_targets_k(
-            Graph& graph,
-            const typename Graph::vertex_descriptor& vertex,
-            ObservedPropertyMap& observed,
-            size_t max_unobserved = 1)
-    {
-        if (!observed[vertex]) return {};
-        std::vector<typename Graph::vertex_descriptor> targets;
-        for (const auto& w: range_pair(adjacent_vertices(vertex, graph))) {
-            if (!observed[w]) {
-                if (targets.size() >= max_unobserved) {
-                    return {};
-                } else {
-                    targets.push_back(w);
-                }
+template<typename T> struct PrintType;
+template<class Graph, class ActivePropertyMap, class ObservedPropertyMap>
+bool dominate(const Graph& graph, const ActivePropertyMap& active, ObservedPropertyMap& observed) {
+    typename boost::property_map<Graph, boost::vertex_index_t>::type index = get(boost::vertex_index, graph);
+    std::for_each(vertices(graph).first, vertices(graph).second, [&] (const auto& v) {
+        auto x = index[v];
+
+        if (active[v] == PmuState::Active) {
+            observed[v] = true;
+            auto adjacent = boost::adjacent_vertices(v, graph);
+            for (auto it = adjacent.first; it != adjacent.second; ++it) {
+                auto windex = index[*it];
+                observed[*it] = true;
             }
         }
-        return targets;
+    });
+    return true;
+}
+
+template<class Graph, class ObservedPropertyMap>
+std::vector<typename Graph::vertex_descriptor> propagation_targets_k(
+        Graph& graph,
+        const typename Graph::vertex_descriptor& vertex,
+        ObservedPropertyMap& observed,
+        size_t max_unobserved = 1)
+{
+    if (!observed[vertex]) return {};
+    std::vector<typename Graph::vertex_descriptor> targets;
+    for (const auto& w: range_pair(adjacent_vertices(vertex, graph))) {
+        if (!observed[w]) {
+            if (targets.size() >= max_unobserved) {
+                return {};
+            } else {
+                targets.push_back(w);
+            }
+        }
     }
+    return targets;
+}
 
-    template<class Graph, class ObservedPropertyMap>
-    bool propagate(const Graph& graph, ObservedPropertyMap& observed, size_t max_unobserved = 1) {
-        typename boost::property_map<Graph, boost::vertex_index_t>::type index = get(boost::vertex_index, graph);
-        boost::vector_property_map<size_t, decltype(index)> unobserved_degree(num_vertices(graph), index);
-        std::vector<typename Graph::vertex_descriptor> queue;
-        for (const auto& v: range_pair(vertices(graph))) {
-            if (!observed[v]) {
-                for (const auto& w: range_pair(adjacent_vertices(v, graph))) {
-                    unobserved_degree[w] += 1;
-                }
-            }
-        }
-        for (const auto& v: range_pair(vertices(graph))) {
-            if (observed[v] && unobserved_degree[v] > 0 && unobserved_degree[v] <= max_unobserved) {
-                queue.push_back(v);
-            }
-        }
-        while (!queue.empty()) {
-            auto v = queue.back();
-            queue.pop_back();
+template<class Graph, class ObservedPropertyMap>
+bool propagate(const Graph& graph, ObservedPropertyMap& observed, size_t max_unobserved = 1) {
+    typename boost::property_map<Graph, boost::vertex_index_t>::type index = get(boost::vertex_index, graph);
+    boost::vector_property_map<size_t, decltype(index)> unobserved_degree(num_vertices(graph), index);
+    std::vector<typename Graph::vertex_descriptor> queue;
+    for (const auto& v: range_pair(vertices(graph))) {
+        if (!observed[v]) {
             for (const auto& w: range_pair(adjacent_vertices(v, graph))) {
-                if (!observed[w]) {
-                    observed[w] = true;
-                    if (observed[w] && unobserved_degree[w] > 0 && unobserved_degree[w] <= max_unobserved) {
-                        queue.push_back(w);
-                    }
-                    for (const auto& u: range_pair(adjacent_vertices(w, graph))) {
-                        unobserved_degree[u] -= 1;
-                        auto deg = unobserved_degree[u];
-                        if (observed[u] && deg > 0 && deg == max_unobserved) {
-                            queue.push_back(u);
-                        }
+                unobserved_degree[w] += 1;
+            }
+        }
+    }
+    for (const auto& v: range_pair(vertices(graph))) {
+        if (observed[v] && unobserved_degree[v] > 0 && unobserved_degree[v] <= max_unobserved) {
+            queue.push_back(v);
+        }
+    }
+    while (!queue.empty()) {
+        auto v = queue.back();
+        queue.pop_back();
+        for (const auto& w: range_pair(adjacent_vertices(v, graph))) {
+            if (!observed[w]) {
+                observed[w] = true;
+                if (observed[w] && unobserved_degree[w] > 0 && unobserved_degree[w] <= max_unobserved) {
+                    queue.push_back(w);
+                }
+                for (const auto& u: range_pair(adjacent_vertices(w, graph))) {
+                    unobserved_degree[u] -= 1;
+                    auto deg = unobserved_degree[u];
+                    if (observed[u] && deg > 0 && deg == max_unobserved) {
+                        queue.push_back(u);
                     }
                 }
             }
         }
-        return true;
     }
+    return true;
+}
 
-    template<class Graph, class ObservedPropertyMap>
-    bool observed(const Graph& graph, const ObservedPropertyMap& observed) {
-        return std::all_of(boost::vertices(graph).first, boost::vertices(graph).second, [&] (const auto& v) {
-            return observed[v];
-        });
-    }
+template<class Graph, class ObservedPropertyMap>
+bool observed(const Graph& graph, const ObservedPropertyMap& observed) {
+    return std::all_of(boost::vertices(graph).first, boost::vertices(graph).second, [&] (const auto& v) {
+        return observed[v];
+    });
+}
 }
 
 #endif //PDS_PDS_HPP
--- main.cpp
+++ main.cpp
@@ -18,24 +18,6 @@ void processOgdf(const std::string& filename) {
     std::cout << "attributes: " << attr.attributes() << std::endl;
 }
 
-struct Bus {
-    bool zero_injection;
-};
-
-template<class T>
-class WrapBool {
-    bool& source;
-public:
-    explicit WrapBool(bool& source) : source(source) { }
-    WrapBool& operator=(const T& other) {
-        source = bool(other);
-        return *this;
-    }
-    operator T() {
-        return T(source);
-    }
-};
-
 template<class T, class Graph>
 auto make_vector_property_map(T&& vec, Graph graph) {
     return boost::make_iterator_property_map(vec.begin(), get(boost::vertex_index, graph));
@@ -43,41 +25,26 @@ auto make_vector_property_map(T&& vec, Graph graph) {
 
 void processBoost(const std::string& filename)
 {
-    std::ifstream graph_in(filename);
-    boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, Bus> graph;
-    using vertex_id_t = decltype(graph)::vertex_descriptor;
-    using edge_id_t = decltype(graph)::edge_descriptor;
-    //std::vector<long> long_zi(boost::num_vertices(graph));
-    boost::dynamic_properties attr(boost::ignore_other_properties);
-    std::vector<long> long_zi(num_vertices(graph));
-    typename boost::property_map<decltype(graph), boost::vertex_index_t>::type index = get(boost::vertex_index, graph);
-    boost::vector_property_map<long, decltype(index)> zero_injection(num_vertices(graph), index);
-    attr.property("zero_injection", zero_injection);//make_vector_property_map(long_zi, graph));
-    boost::read_graphml(graph_in, graph, attr);
-    graph_in.close();
+    auto graph = pds::read_graphml(filename);
     for (auto it = vertices(graph).first; it != vertices(graph).second; ++it) {
         auto v = *it;
         graph[v].zero_injection = true;//zero_injection[v];
     }
     std::cout << "n=" << boost::num_vertices(graph) << ", m=" << boost::num_edges(graph) << std::endl;
-    std::cout << "attributes: " << std::distance(attr.begin(), attr.end()) << std::endl;
-    for (auto a: attr) {
-        std::cout << "found attribute " << a.first << std::endl;
-    }
     //std::vector<pds::PmuState> active(boost::num_vertices(graph));
     //std::vector<uint8_t> observed(boost::num_vertices(graph));
     //auto active_map = make_vector_property_map(active, graph);
     //auto observed_map = make_vector_property_map(observed, graph);
     {
-        boost::vector_property_map<pds::PmuState, decltype(index)> active_map(num_vertices(graph), index);
-        boost::vector_property_map<uint8_t, decltype(index)> observed_map(num_vertices(graph), index);
-        pds::solve_pds(graph, active_map, observed_map);
-        pds::dominate(graph, active_map, observed_map);
+        boost::associative_property_map<std::map<decltype(graph)::vertex_descriptor, pds::PmuState>> active;
+        boost::associative_property_map<std::map<decltype(graph)::vertex_descriptor, bool>> observed;
+        pds::solve_pds(graph, active, observed);
+        pds::dominate(graph, active, observed);
         {
             bool sep = false;
             std::cout << "active vertices: ";
             for (const auto &v: pds::range_pair(vertices(graph))) {
-                if (active_map[v] == pds::PmuState::Active) {
+                if (get(active, v) == pds::PmuState::Active) {
                     if (sep) std::cout << ", ";
                     sep = true;
                     std::cout << v;
@@ -85,20 +52,20 @@ void processBoost(const std::string& filename)
             }
         }
         std::cout << std::endl;
-        pds::propagate(graph, observed_map);
-        std::cout << "fully observed: " << pds::observed(graph, observed_map) << std::endl;
+        pds::propagate(graph, observed);
+        std::cout << "fully observed: " << pds::observed(graph, observed) << std::endl;
     }
 
     {
         std::cout << "take 2:" << std::endl;
-        boost::vector_property_map<pds::PmuState, decltype(index)> active_map(num_vertices(graph), index);
-        boost::vector_property_map<uint8_t, decltype(index)> observed_map(num_vertices(graph), index);
+        boost::associative_property_map<std::map<decltype(graph)::vertex_descriptor, pds::PmuState>> active;
+        boost::associative_property_map<std::map<decltype(graph)::vertex_descriptor, bool>> observed;
         for (auto i: std::vector{5, 11, 55}) {
-            active_map[i] = pds::PmuState::Active;
+            active[i] = pds::PmuState::Active;
         }
-        pds::dominate(graph, active_map, observed_map);
-        pds::propagate(graph, observed_map);
-        std::cout << "fully observed: " << pds::observed(graph, observed_map) << std::endl;
+        pds::dominate(graph, active, observed);
+        pds::propagate(graph, observed);
+        std::cout << "fully observed: " << pds::observed(graph, observed) << std::endl;
     }
 }
 
