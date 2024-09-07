7b6a83dc8b8fe2c93e527732b05fa4633c75ae17
Mauro Lucci <maurolucci.14@gmail.com>
2024-09-07 13:01:11 -0300

    Fix: Quite arguento no usado en modelBrimkov

--- src/pds/gurobi_solve.cpp
+++ src/pds/gurobi_solve.cpp
@@ -395,7 +395,7 @@ MIPModel modelBrimkovExpanded(PdsState& state) {
     return mipmodel;
 }
 
-MIPModel modelBrimkov(PdsState& state, bool geq) {
+MIPModel modelBrimkov(PdsState& state) {
     MIPModel mipmodel;
     auto& model = *mipmodel.model;
     auto& xi = mipmodel.xi;
@@ -424,4 +424,151 @@ MIPModel modelBrimkov(PdsState& state, bool geq) {
         GRBLinExpr observers = 0;
         observers += xi.at(u);
         for (auto e: state.graph().inEdges(u)) {
-            auto v = state.graph().
+            auto v = state.graph().source(e);
+            assert(v != u);
+            observers += ye.at(e);
+        }
+        model.addConstr(observers == 1);
+        for (auto e: state.graph().outEdges(u)) {
+            auto v = state.graph().target(e);
+            // s_u - s_v + (T + 1) y_{uv} <= T (4) e@(u,v) \in E'
+            model.addConstr(si.at(u) - si.at(v) + (T + 1) * ye.at(e) <= T);
+            for (auto w: state.graph().neighbors(u)) {
+                if (v != w) {
+                    unused(u, w, v, e);
+                    // s_w - s_v + (T + 1) y_e <= T + (T+1) x_u , e@(u,v) \in E', w \in N(u) - v
+                    model.addConstr(si.at(w) - si.at(v) + (T + 1) * ye.at(e) <= T + (T+1) * xi.at(u));
+                }
+            }
+        }
+    }
+    return mipmodel;
+}
+
+MIPModel modelBrimkovGeq(PdsState& state) {
+    MIPModel mipmodel;
+    auto& model = *mipmodel.model;
+    auto& xi = mipmodel.xi;
+    map <PowerGrid::VertexDescriptor, GRBVar> si;
+    map <PowerGrid::EdgeDescriptor, GRBVar> ye;
+    size_t T = state.graph().numVertices();
+    for (auto v: state.graph().vertices()) {
+        xi.try_emplace(v, model.addVar(0.0, 1.0, 1.0, GRB_BINARY, "x"));
+        si.try_emplace(v, model.addVar(0.0, static_cast<double>(T), 0.0, GRB_INTEGER, "s"));
+        for (auto e: state.graph().outEdges(v)) {
+            assert(!ye.contains(e));
+            ye.try_emplace(e, model.addVar(0.0, 1.0, 0.0, GRB_BINARY));
+            if (!state.isZeroInjection(v)) {
+                model.addConstr(ye.at(e) == 0.0);
+            }
+        }
+        if (state.isActive(v)) {
+            model.addConstr(xi.at(v) == 1.0);
+        }
+        else if (state.isInactive(v)) {
+            model.addConstr(xi.at(v) == 0.0);
+        }
+    }
+    for (auto u: state.graph().vertices()) {
+        // x_u + \sum_{v \in N(u)} y_{vu} >= 1 (3)
+        GRBLinExpr observers = 0;
+        observers += xi.at(u);
+        for (auto e: state.graph().inEdges(u)) {
+            auto v = state.graph().source(e);
+            assert(v != u);
+            observers += ye.at(e);
+        }
+        model.addConstr(observers >= 1);
+        for (auto e: state.graph().outEdges(u)) {
+            auto v = state.graph().target(e);
+            // s_u - s_v + (T + 1) y_{uv} <= T (4) e@(u,v) \in E'
+            model.addConstr(si.at(u) - si.at(v) + (T + 1) * ye.at(e) <= T);
+            for (auto w: state.graph().neighbors(u)) {
+                if (v != w) {
+                    unused(u, w, v, e);
+                    // s_w - s_v + (T + 1) y_e <= T + (T+1) x_u , e@(u,v) \in E', w \in N(u) - v
+                    model.addConstr(si.at(w) - si.at(v) + (T + 1) * ye.at(e) <= T + (T+1) * xi.at(u));
+                }
+            }
+        }
+    }
+    return mipmodel;
+}
+
+MIPModel modelBrimkovAlt(PdsState& state) {
+    MIPModel mipmodel;
+    auto& model = *mipmodel.model;
+    auto& xi = mipmodel.xi;
+    map <PowerGrid::VertexDescriptor, GRBVar> si;
+    map <PowerGrid::EdgeDescriptor, GRBVar> ye;
+    size_t T = state.graph().numVertices();
+    for (auto v: state.graph().vertices()) {
+        xi.try_emplace(v, model.addVar(0.0, 1.0, 1.0, GRB_BINARY, "x"));
+        si.try_emplace(v, model.addVar(0.0, static_cast<double>(T), 0.0, GRB_INTEGER, "s"));
+        for (auto e: state.graph().outEdges(v)) {
+            assert(!ye.contains(e));
+            ye.try_emplace(e, model.addVar(0.0, 1.0, 0.0, GRB_BINARY));
+            if (!state.isZeroInjection(v)) {
+                model.addConstr(ye.at(e) == 0.0);
+            }
+        }
+        if (state.isActive(v)) {
+            model.addConstr(xi.at(v) == 1.0);
+        }
+        else if (state.isInactive(v)) {
+            model.addConstr(xi.at(v) == 0.0);
+        }
+    }
+    for (auto u: state.graph().vertices()) {
+        // x_u + \sum_{v \in N(u)} (x_v + y_{vu}) >= 1 (3)
+        GRBLinExpr observers = 0;
+        observers += xi.at(u);
+        for (auto e: state.graph().inEdges(u)) {
+            auto v = state.graph().source(e);
+            assert(v != u);
+            observers += ye.at(e);
+            observers += xi.at(v);
+        }
+        model.addConstr(observers >= 1);
+        for (auto e: state.graph().outEdges(u)) {
+            auto v = state.graph().target(e);
+            // s_u - s_v + (T + 1) y_e <= T, e@(u,v) \in E'
+            model.addConstr(si.at(u) - si.at(v) + (T + 1) * ye.at(e) <= T;
+            for (auto w: state.graph().neighbors(u)) {
+                if (v != w) {
+                    unused(u, w, v, e);
+                    // s_w - s_v + (T + 1) y_e <= T, e@(u,v) \in E', w \in N(u) - v
+                    model.addConstr(si.at(w) - si.at(v) + (T + 1) * ye.at(e) <= T;
+                }
+            }
+        }
+    }
+    return mipmodel;
+}
+
+MIPModel modelAzamiBrimkov(PdsState& state) {
+    MIPModel mipmodel;
+    auto& model = *mipmodel.model;
+    auto &graph = static_cast<const PdsState &>(state).graph();
+    auto& xi = mipmodel.xi;
+    map <std::pair<size_t, PowerGrid::VertexDescriptor>, GRBVar> si;
+    map <std::pair<PowerGrid::VertexDescriptor, PowerGrid::VertexDescriptor>, GRBVar> ye;
+    size_t T = graph.numVertices();
+    for (auto v: graph.vertices()) {
+        xi.try_emplace(v, model.addVar(0.0, 1.0, 1.0, GRB_BINARY));
+        GRBLinExpr vObserved;
+        for (size_t i = 0; i < T; ++i) {
+            si.try_emplace({i, v}, model.addVar(0.0, 1.0, 0.0, GRB_BINARY));
+            vObserved += si[{i, v}];
+        }
+        model.addConstr(vObserved >= 0);
+        for (auto w: graph.neighbors(v)) {
+            ye.try_emplace({v, w}, model.addVar(0.0, 1.0, 0.0, GRB_BINARY));
+        }
+    }
+    struct Unimplemented{ };
+    throw Unimplemented{};
+    return mipmodel;
+}
+
+} // namespace pds
