6ccd3036f9ffc78451659f04a58c0bb12e6c476f
Mauro Lucci <maurolucci.14@gmail.com>
2024-09-26 16:07:37 -0300

    Add: restricciones y_uv + y_vu <= 1

--- src/pds/cycles_solve.cpp
+++ src/pds/cycles_solve.cpp
@@ -347,6 +347,13 @@ SolveResult solveCycles(
             }
         }
 
+        // Constraints (3)
+        for (auto e: state.graph().edges()) {
+            auto u = state.graph().source(e);
+            auto v = state.graph().target(e);
+            model.addConstr(ye.at(u).at(v) + ye.at(v).at(u) <= 1);
+        }
+
         // Add callback
         Callback cb(lowerBound, upperBound, sv, ye, state, lastSolution, 
                     feasibleSolution, blankVertices, earlyStop, intermediateCycles, cycles, seen, output);
